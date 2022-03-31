0900040ad6f15c6b86e6b7799744d890db357305
Qutfullo Ochilov <qutfulloo@silkroadprofessionals.com>
2022-03-31 11:36:28 +0500

    add bad codes for testing

--- TestSonarcloud/Services/WeatherForecastService.cs
+++ TestSonarcloud/Services/WeatherForecastService.cs
@@ -15,26 +15,47 @@ namespace TestSonarcloud.Services
 
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var rng = new Random(); ;
+                ;
+
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+            }
+            catch (Exception ex)
+            {
+                var exception = ex;
+            }
+
+            return Enumerable.Empty<WeatherForecast>();
         }
 
         public IEnumerable<WeatherForecast> Get1()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+            }
+            catch
+            {
+
+            }
+            return Enumerable.Empty<WeatherForecast>(); ;
         }
 
         public IEnumerable<WeatherForecast> Get2()
